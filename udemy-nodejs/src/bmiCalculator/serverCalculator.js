d89863ac006650449ec840928201cfb406064b39
Satria-WS <extencezone02@gmail.com>
2022-06-24 09:22:30 +0700

    if statement
    create checking

--- udemy-nodejs/src/bmiCalculator/serverCalculator.js
+++ udemy-nodejs/src/bmiCalculator/serverCalculator.js
@@ -10,37 +10,62 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   //##just check console
   //res.send("this is my bmi calculator");
-
   res.sendFile(path.join(__dirname, "bmiCalculator.html"));
 });
 
 app.post("/", (req, res) => {
   //##check with log req.body
   //console.log(req.body);
-
   const objectBMI = {
     weight: req.body.weight,
     height: req.body.height,
   };
-
   //##store on variable
   const weight = objectBMI.weight;
   const height = objectBMI.height;
-  console.log(weight, height);
+  // console.log(weight, height); // 70 170
 
   //##create function calculate BMI CALCULATOR
   function calculateBMI(weight, height) {
-    const calculate = weight / Math.pow(height/100, 2);
-    console.log(Math.floor(calculate)) 
+    const calculate = weight / Math.pow(height / 100, 2);
+    // console.log(Math.floor(calculate));
+    return Math.floor(calculate);
   }
   calculateBMI(weight, height);
 
-  res.send("succesfull");
+  //## checking
+  if (calculateBMI(weight, height) === 24) {
+    console.log("Weight: ", weight);
+    console.log("Height: ", height);
+    console.log("ideal: ", true);
+    res.send("lo ideal");
+  } else {
+    console.log("Weight: ", weight);
+    console.log("Height: ", height);
+    console.log("ideal: ", false);
+    res.send("lo gendut goblok gendut anjing idiot");
+  }
 });
 
+//## listen port
 app.listen(port, () => {
   console.log(`port: ${port} was succesfull`);
 });
 
-//exponen
+//##exponen
 // Math.pow(height,2)
+
+//##check function
+/* 
+function check1(number1) {
+  const number9 = number1;
+  return number9;
+}
+check1();
+
+if (check1(9) === 9) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+ */
