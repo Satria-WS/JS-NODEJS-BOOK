0f5a47241999338d472ac6eb3f02de8e78a7b820
Satria-WS <extencezone02@gmail.com>
2022-06-29 10:28:20 +0700

    fix add api
    fix add api

--- udemy-nodejs/src/weatherAPI/teori JSON.md
+++ udemy-nodejs/src/weatherAPI/teori JSON.md
@@ -2,3 +2,8 @@ JSON
 
 - JSON is currently the most favoured format , because its much lighter weight than the other two
 - and also because its very easy to turn back into a javascript object
+
+- Why request module depreciated , 
+https://nodesource.com/blog/express-going-into-maintenance-mode
+creating a major change in the patterns would affect the more than 41000 modules which depend on this module.
+In short, major changes to request would result in a lot of conflicts. That’s why the contributors to request opted to let the code and the community to outgrow the module organically
--- udemy-nodejs/src/weatherAPI/weather.js
+++ udemy-nodejs/src/weatherAPI/weather.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const app = express();
+const https = require("https");
 
-const port = 3000;
+const port =  3000;
 
-app.get("/" , (req,res)=> {
-    res.send("Server is up and running.")
-})
+app.get("/", (req, res) => {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=-6.413184&lon=106.794645&units=metric&appid=03541cac39d73f6a1fe3683c8ac05c3d&mode=json&lang=id&q=Depok";
 
+  https.get(url, (response) => {
+    console.log(response);
+  });
 
+  res.send("Server is up and running");
+});
 
-
-
-
-
-
-
-
-
-
-
-app.listen(port, ()=> {
-    console.log("Server is running on port " + port)
-})
+app.listen(port, function () {
+  console.log("Server is running on port " + port);
+});
