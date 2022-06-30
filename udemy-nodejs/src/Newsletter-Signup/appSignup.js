740a4cb25c5b9a77a730a25ab61c46a5c83d15ee
Satria-WS <extencezone02@gmail.com>
2022-06-30 15:33:35 +0700

    report finish lanjut besok
    kwkwkw

--- udemy-nodejs/src/Newsletter-Signup/appSignup.js
+++ udemy-nodejs/src/Newsletter-Signup/appSignup.js
@@ -4,17 +4,28 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
 
-
 const port = 8080;
 
-//##static assets
+//##static assets--------------------------------------------------------------------------
+//readed public folder as static
 app.use(express.static(path.join(__dirname, "public")));
+
+//##bodyparser/urlencoded--------------------------------------------------------------------------
+app.use(bodyParser.urlencoded({extended:true}))
 
 //##get--------------------------------------------------------------------------
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "signup.html"));
 });
-
+//##post--------------------------------------------------------------------------
+app.post("/formSignup" , (req,res)=> {
+  const userSignup = {
+    fName: req.body.fName,
+    lName: req.body.lName,
+    email:req.body.email
+   };
+   console.log(userSignup);
+})
 
 //##listen--------------------------------------------------------------------------
 app.listen(port, () => {
