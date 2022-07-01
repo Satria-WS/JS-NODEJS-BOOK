0ddff57c30fa65dd06a024829d2ecf1e1b56b681
Satria-WS <extencezone02@gmail.com>
2022-07-01 18:03:21 +0700

    add mailChimp

--- udemy-nodejs/src/Newsletter-Signup/appSignup.js
+++ udemy-nodejs/src/Newsletter-Signup/appSignup.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const port = 8080;
 
@@ -11,21 +12,28 @@ const port = 8080;
 app.use(express.static(path.join(__dirname, "public")));
 
 //##bodyparser/urlencoded--------------------------------------------------------------------------
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //##get--------------------------------------------------------------------------
 app.get("/", (req, res) => {
+  //mailchimp object
+  mailchimp.setConfig({
+    apiKey: "5e72fb86a74e20bab950850bdf3d5a21-us14",
+    server: "us14",
+  });
   res.sendFile(path.join(__dirname, "signup.html"));
 });
 //##post--------------------------------------------------------------------------
-app.post("/formSignup" , (req,res)=> {
+app.post("/formSignup", (req, res) => {
   const userSignup = {
     fName: req.body.fName,
     lName: req.body.lName,
-    email:req.body.email
-   };
-   console.log(userSignup);
-})
+    email: req.body.email,
+  };
+  console.log(userSignup);
+});
+
+
 
 //##listen--------------------------------------------------------------------------
 app.listen(port, () => {
